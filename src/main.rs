373f9c9069977b5b39024a807427dd3a54cecd2d
Dimitri De Frenne <ddefrenne@gmail.com>
2015-09-30 13:18:18 +0200

    Prefering an i32 to an Array seems the easier way for this case

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 fn main() {
     let rows: [char; 10] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K'];
-    let columns: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9,  10];
+    let amount_of_columns = 10;
 
-    let grid = build_grid(&rows, &columns);
+    let grid = build_grid(&rows, amount_of_columns);
 }
 
-fn build_grid(rows: &[char; 10], columns: &[i32; 10]) {
-    let line = std::iter::repeat("------").take(10).collect::<String>().to_string();
+fn build_grid(rows: &[char; 10], amount_of_columns: i32) {
+    // take() takes a usize, so we need to cast the i32 to a usize: http://stackoverflow.com/a/28601386
+    let line = std::iter::repeat("------").take(amount_of_columns as usize)
+                                          .collect::<String>()
+                                          .to_string();
 
-    display_upper_row(&line, *columns);
+    display_upper_row(&line, amount_of_columns);
 }
 
-fn display_upper_row(line: &str, columns: [i32; 10]) {
+fn display_upper_row(line: &str, amount_of_columns: i32) {
     let mut parts: Vec<String> = vec![];
-    for column in &columns {
+    for column in (1..amount_of_columns + 1) {
         parts.push(column.to_string());
     }
     let mut column_part = format!("|  {}  |", parts.join("  |  ")); // String, because it needs to be growable
